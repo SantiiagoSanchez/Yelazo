b2f8bdfcc6cd614ca65fb783651d91706e4a9994
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-06-30 11:04:17 -0300

    Carpeta Servicios

---
+++ Yelazo/Yelazo.Client/Servicios/HttpServicio.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Text.Json;
+
+namespace Yelazo.Client.Servicios
+{
+    public class HttpServicio : IHttpServicio
+    {
+        private readonly HttpClient http;
+
+        public HttpServicio(HttpClient http)
+        {
+            this.http = http;
+        }
+
+        public async Task<HttpRespuesta<T>> Get<T>(string url)
+        {
+            var response = await http.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var respuesta = await DesSerializar<T>(response);
+                return new HttpRespuesta<T>(respuesta, false, response);
+            }
+            else
+            {
+                return new HttpRespuesta<T>(default, true, response);
+            }
+        }
+
+        public async Task<HttpRespuesta<object>> Post<T>(string url, T entidad)
+        {
+            var EnviarJSON = JsonSerializer.Serialize(entidad);
+            var EnviarCONTENIDO = new StringContent(EnviarJSON, Encoding.UTF8, "application/json");
+            var Response = await http.PostAsync(url, EnviarCONTENIDO);
+            if (Response.IsSuccessStatusCode)
+            {
+                var Respuesta = DesSerializar<object>(Response);
+                return new HttpRespuesta<object>(Respuesta, false, Response);
+            }
+            else
+            {
+                return new HttpRespuesta<object>(default, true, Response);
+            }
+        }
+
+        public async Task<HttpRespuesta<object>> Put<T>(string url, T entidad)
+        {
+            var EnviarJson = JsonSerializer.Serialize(entidad);
+
+            var EnviarContenido = new StringContent(EnviarJson, Encoding.UTF8, "application/json");
+
+            var Response = await http.PutAsync(url, EnviarContenido);
+
+            if (Response.IsSuccessStatusCode)
+            {
+                //var Respuesta = await DesSerializar<object>(Response);
+                return new HttpRespuesta<object>(null, false, Response);
+            }
+            else
+            {
+                return new HttpRespuesta<object>(default, true, Response);
+            }
+        }
+
+        private async Task<T?> DesSerializar<T>(HttpResponseMessage response)
+        {
+            var RespuestaSTR = await response.Content.ReadAsStringAsync();
+            return JsonSerializer.Deserialize<T>(RespuestaSTR, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+        }
+    }
+}
+
---
+++ Yelazo/Yelazo.Client/Servicios/IHttpServicio.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Yelazo.Client.Servicios
+{
+    public interface IHttpServicio
+    {
+        Task<HttpRespuesta<T>> Get<T>(string url);
+        Task<HttpRespuesta<object>> Post<T>(string url, T entidad);
+        Task<HttpRespuesta<object>> Put<T>(string url, T entidad);
+    }
+}
