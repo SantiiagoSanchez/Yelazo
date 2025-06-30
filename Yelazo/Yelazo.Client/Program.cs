0932491916714d2375f1c9c0fed2feb01bcdae02
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-06-30 11:13:29 -0300

    Ahora si

--- Yelazo/Yelazo.Client/Program.cs
+++ Yelazo/Yelazo.Client/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Yelazo.Client;
+using Yelazo.Client.Servicios;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IHttpServicio, HttpServicio>();
 
 await builder.Build().RunAsync();
