165a8d78983d558fc3b97d3dda53616f84bdf93b
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-08-03 15:57:05 -0300

    Entidades faltantes sin FK

--- Yelazo/Yelazo.BD/Data/Context.cs
+++ Yelazo/Yelazo.BD/Data/Context.cs
@@ -11,6 +11,10 @@ namespace Yelazo.BD.Data
     public class Context : DbContext
     {
 
+        public DbSet<CategoriaInsumo> CategoriaInsumos { get; set; }
+        public DbSet<TipoGasto> TiposGasto { get; set; }
+        public DbSet<MetodoPago> MetodosPago { get; set; }
+        public DbSet<Mantenimiento> Mantenimientos { get; set; }
         public DbSet<Producto> Productos { get; set; }
 
         public DbSet<Rol> Roles { get; set; }
---
+++ Yelazo/Yelazo.BD/Data/Entity/MetodoPago.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yelazo.BD.Data.Entity
+{
+    public class MetodoPago : EntityBase
+    {
+        [Required(ErrorMessage = "El nombre del Metodo de pago es obligatorio")]
+        public string Nombre { get; set; }
+    }
+}
